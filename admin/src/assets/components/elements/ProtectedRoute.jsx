99b9b8568b581e759db6fc8528bcd1eaa8d007ad
Monu Giri <whomonugiri@gmail.com>
2025-06-09 14:12:03 +0530

    bg

--- admin/src/assets/components/elements/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, useNavigate } from "react-router-dom";
-
-export const ProtectedRoute = ({ isAuth, children }) => {
-  if (!isAuth) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return children;
-};
--- admin/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const HOST = "https://server.ludokingo.in";
-// export const HOST = "http://localhost:3142";
-
-export const CLIENT = "https://admin.ludokingo.in";
